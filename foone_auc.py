960284a88d69b5bd3ccad47142474625d40642b9
Adoria298 <bananaorange29@outlook.com>
2022-02-15 14:40:20 +0000

    Fixed the unhashable list bug by not hashing a list.

--- foone_auc.py
+++ foone_auc.py
@@ -2,19 +2,16 @@
 # summary: AUC dates based on the founding date of the nearest city from Wikipedia
 import geocoder
 import wikipedia
-from functools import cache
-from pprint import pprint
 
 def get_current_location(): #
     g = geocoder.ip("me")
     return g
 
-@cache
 def disambiguate_wiki(disamb_links, gc) -> str and int: # str is the page and int is its ranking for recursion
     temp_links = []
     # let's get rid of the most unlikely candidates
     for page in disamb_links:
-        if gc.city not in page and gc.city not in summary:
+        if gc.city not in page:
             continue
         if ", " not in page:
             continue
@@ -33,7 +30,7 @@ def disambiguate_wiki(disamb_links, gc) -> str and int: # str is the page and in
             continue
         wpage = wikipedia.page(page) # try and load the page here to check the coordinates
         wlat, wlong = wpage.coordinates
-        lat, long = gc.latlong
+        lat, long = gc.latlng
         if int(lat) - 2 > int(wlat) or int(wlat) > int(lat) + 2: # convert to int to slightly increase range
             ranking[page] -= 1000
         if int(long) - 2 > int(wlong) or int(wlong) > int(long) + 2:
@@ -51,7 +48,7 @@ def disambiguate_wiki(disamb_links, gc) -> str and int: # str is the page and in
         if gc.state in page or gc.state in summary: # allows for England being in summary not titles
             ranking[page] += 2 # no counties
     top = max(ranking, key=lambda x: ranking[x])
-    pprint(ranking)
+    #pprint(ranking)
     return top, ranking[top]
 
 def get_wikipedia_article(gc):
@@ -63,14 +60,14 @@ def get_wikipedia_article(gc):
         elif len(results) == 1:
             return wikipedia.page(results[0]) # .page raises DisambiguationError if needed
         else:
-            wpage = disambiguate_wiki(results, gc)
-            return wikipedia.page(wpage)
+            raise wikipedia.exceptions.DisambiguationError(gc.city, results) # used to avoid code reuse
     except wikipedia.exceptions.DisambiguationError as e:
-        return wikipedia.page(disambiguate_wiki(e.options, gc)[0])
+        wpage = disambiguate_wiki(e.options, gc)
+        return wikipedia.page(wpage[0])
 
 def get_founding_date(article):
     #TODO: get founding date from article by looking for "Founded"/"Settled"/"Incorporated" from the infobox
-    #raise error if none found? or scan summary/history for one?
+    # returns 753 (B.C) if none is found
     # wptools may be useful here
     return 753 # Rome's founding date so the default for AUC if all else fails
 
